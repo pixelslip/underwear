738c2140289cb57caa5fbb97a3da0242e1c11406
Pierre Boumal <pixelslip@gmail.com>
2023-01-14 19:57:48 +0100

    remove glob

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,13 @@ const path = require('path')
 const webpack = require('webpack')
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'js')
-const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
 const dirNode = 'node_modules'
 
@@ -24,7 +21,6 @@ module.exports = {
   resolve: {
     modules: [
       dirApp,
-      dirShared,
       dirStyles,
       dirNode
     ]
@@ -34,31 +30,10 @@ module.exports = {
       IS_DEVELOPMENT
     }),
 
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: './shared',
-          to: ''
-        }
-      ]
-    }),
-
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
     }),
-
-    // new ImageMinimizerPlugin({
-    //   minimizer: {
-    //     options: {
-    //       plugins: [
-    //         ['gifsicle', { interlaced: true }],
-    //         ['jpegtran', { progressive: true }],
-    //         ['optipng', { optimizationLevel: 8 }]
-    //       ]
-    //     }
-    //   }
-    // }),
 
     new HtmlWebpackPlugin({
       template: 'index.html'
