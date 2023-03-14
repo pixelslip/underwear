772ef5a40442c33c3928b7057c9446e1ad5b9619
Pierre Boumal <pixelslip@gmail.com>
2023-03-14 16:09:55 +0100

    update starter for threejs

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,72 @@
-console.log('You need to change your slip!')
-import Scene from './Scene'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import GUI from 'lil-gui';
 
-// let s = new Scene();
-// s.animate();
+// const gui = new GUI();
+const clock = new THREE.Clock()
+const canvas = document.querySelector('.canvas')
+/** Stats */
+// const stats = Stats()
+// document.body.appendChild(stats.dom)
+
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
+camera.position.z = 5;
+
+/** Controls */
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize( sizes.width, sizes.height );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('resize', () =>
+{
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+
+  // pixel ratio
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Material
+// const textureLoader = new THREE.TextureLoader()
+const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
+
+// Geometry
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry( 1, 1, 1 ),
+  material );
+scene.add( cube );
+
+
+
+const animate = () => {
+  const elapsed = clock.getElapsedTime()
+
+  cube.rotation.x = elapsed * 0.5;
+  cube.rotation.y = elapsed * 0.5;
+
+  controls.update()
+  // stats.update()
+  renderer.render( scene, camera );
+
+  requestAnimationFrame( animate );
+}
+
+animate();
